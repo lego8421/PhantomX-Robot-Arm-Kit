162990ee30339b048e0c6f4e37aa4d1ffbf1e4e4
matt <lego8421@gmail.com>
2018-03-14 23:42:48 +0900

    done generateJointAnglePacket function

--- PhantomX-Robot-Arm-Kit/dynamixel/dynamixel.cpp
+++ PhantomX-Robot-Arm-Kit/dynamixel/dynamixel.cpp
@@ -2,8 +2,7 @@
 
 #include <QDebug>
 
-Dynamixel::Dynamixel(Dynamixel::type type)
-{
+Dynamixel::Dynamixel(Dynamixel::type type) {
 
     _info.type = type;
 
@@ -16,52 +15,85 @@ Dynamixel::Dynamixel(Dynamixel::type type)
     }
 }
 
-QByteArray Dynamixel::generateJointAnglePacket(dVector &q) {
+QByteArray Dynamixel::generateJointAnglePacket(dVector q) {
 
     QByteArray buffer;
     uint16_t parameter[5] = {0,};
-    int parameterLength = 15;
+    uint8_t parameterLength = 5 * (5+2) + 4;
+    uint8_t checkSum = 0;
 
-    buffer[0] = 0xFF;
-    buffer[1] = 0xFF;
-
-    buffer[2] = 0xFE;
-    buffer[3] = parameterLength + 2;
-    buffer[4] = 0x83;
+    q[1] -= ((ANGLE_OFFSET - LINK_OFFSET) * _DEG2RAD);
+    q[2] += ((ANGLE_OFFSET - LINK_OFFSET) * _DEG2RAD);
 
     for(int i=0;i<5;i++) {
         parameter[i] = convertAngleToDynamixel(q[i] * _RAD2DEG);
     }
 
+    // header
+    buffer[0] = 0xFF;
+    buffer[1] = 0xFF;
+
+    // instruction
+    buffer[2] = 0xFE;
+    buffer[3] = parameterLength;
+    buffer[4] = 0x83;
+    buffer[5] = 0x1E;
+    buffer[6] = 0x04;
+
+
     // yaw
-    buffer[5] = 1;
-    buffer[6] = parameter[0] & 0x00FF;
-    buffer[7] = parameter[0] >> 8;
+    buffer[7] = 0x01;
+    buffer[8] = parameter[0] & 0x00FF;
+    buffer[9] = parameter[0] >> 8;
+    buffer[10] = 0x00;
+    buffer[11] = 0x00;
 
     // pitch 1
-    buffer[8] = 2;
-    buffer[9] = parameter[1] & 0x00FF;
-    buffer[10] = parameter[1] >> 8;
+    buffer[12] = 0x02;
+    buffer[13] = convertDualDynamixel(parameter[1]) & 0x00FF;
+    buffer[14] = convertDualDynamixel(parameter[1]) >> 8;
+    buffer[15] = 0x00;
+    buffer[16] = 0x00;
 
-    buffer[11] = 3;
-    buffer[12] = (_info.valueMax - parameter[1]) & 0x00FF;
-    buffer[13] = (_info.valueMax - parameter[1]) >> 8;
+    buffer[17] = 0x03;
+    buffer[18] = parameter[1] & 0x00FF;
+    buffer[19] = parameter[1] >> 8;
+    buffer[20] = 0x00;
+    buffer[21] = 0x00;
 
+    // pitch 2
+    buffer[22] = 0x04;
+    buffer[23] = parameter[2] & 0x00FF;
+    buffer[24] = parameter[2] >> 8;
+    buffer[25] = 0x00;
+    buffer[26] = 0x00;
 
-    qDebug() << buffer;
+    buffer[27] = 0x05;
+    buffer[28] = convertDualDynamixel(parameter[2]) & 0x00FF;
+    buffer[29] = convertDualDynamixel(parameter[2]) >> 8;
+    buffer[30] = 0x00;
+    buffer[31] = 0x00;
+
+    // pitch 3
+    buffer[32] = 0x06;
+    buffer[33] = parameter[3] & 0x00FF;
+    buffer[34] = parameter[3] >> 8;
+    buffer[35] = 0x00;
+    buffer[36] = 0x00;
+
+    // roll
+    buffer[37] = 0x07;
+    buffer[38] = parameter[4] & 0x00FF;
+    buffer[39] = parameter[4] >> 8;
+    buffer[40] = 0x00;
+    buffer[41] = 0x00;
+
+    for(int i = 2; i < 42; i++) {
+        checkSum += buffer[i];
+    }
+    buffer[42] = ~checkSum;
 
     return buffer;
-
-
-//    unsigned char CheckSum = 0;
-//    unsigned char PacketLength = ParameterLength + 6;
-
-//    for (Count = 2; Count < PacketLength - 1; Count++)  // Except 0xFF, Checksum
-//    {
-//        CheckSum += TxBuffer[Count];
-//    }
-
-//    TxBuffer[Count] = ~CheckSum;
 }
 
 double Dynamixel::convertDynamixelToAngle(uint16_t value) {
@@ -91,4 +123,8 @@ uint16_t Dynamixel::convertAngleToDynamixel(double angle) {
     }
 
     return ret;
+}
+
+uint16_t Dynamixel::convertDualDynamixel(uint16_t value) {
+    return _info.angleMax - value;
 }
--- PhantomX-Robot-Arm-Kit/dynamixel/dynamixel.h
+++ PhantomX-Robot-Arm-Kit/dynamixel/dynamixel.h
@@ -9,7 +9,7 @@ class Dynamixel
 {
 public:
 
-    enum type{
+    enum type {
         DX = 0,
         AX = 1,
         RX = 2,
@@ -25,13 +25,17 @@ public:
 
     Dynamixel(Dynamixel::type type);
 
-    QByteArray generateJointAnglePacket(dVector &q);
+    QByteArray generateJointAnglePacket(dVector q);
 
 
     double convertDynamixelToAngle(uint16_t value);
     uint16_t convertAngleToDynamixel(double angle);
+    uint16_t convertDualDynamixel(uint16_t value);
 
 private:
+    const double LINK_OFFSET = 17.0;
+    const double ANGLE_OFFSET = 90.0;
+
     Info _info;
 };
 
