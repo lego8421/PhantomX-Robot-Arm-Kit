6a6fbac155198f923711daf61cbf7236316d5785
matt <lego8421@gmail.com>
2018-03-19 10:21:33 +0900

    add App version on title

--- PhantomX-Robot-Arm-Kit/main.cpp
+++ PhantomX-Robot-Arm-Kit/main.cpp
@@ -1,10 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#define APP_VERSION "1.0.0"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setApplicationVersion(QString(APP_VERSION));
     MainWindow w;
+    w.setWindowTitle("PhantomX Robot Arm Kit (v" + QString(APP_VERSION) + ")");
     w.show();
 
     return a.exec();
